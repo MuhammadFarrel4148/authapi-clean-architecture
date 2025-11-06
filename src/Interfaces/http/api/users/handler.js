eeecaa18093954698f29f6d5ea762c27f0e381fc
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-11-06 11:32:38 +0700

    revision code and add server

---
+++ src/Interfaces/http/api/users/handler.js
@@ -0,0 +1,30 @@
+const AddUserUseCase = require('../../../../Applications/use_case/AddUserUseCase');
+
+class UsersHandler {
+    constructor(container) {
+        this._container = container;
+        
+        this.postUserHandler = this.postUserHandler.bind(this);
+    };
+
+    async postUserHandler(request, h) {
+        try {
+            const addUserUseCase = this._container.getInstance(AddUserUseCase.name);
+            const addedUser = await addUserUseCase.execute(request.payload);
+
+            const response = h.response({
+                status: 'success',
+                data: {
+                    addedUser
+                }
+            });
+            response.code(201);
+            return response;
+        } catch(error) {
+            console.log(error);
+        }
+        
+    };
+};
+
+module.exports = UsersHandler
